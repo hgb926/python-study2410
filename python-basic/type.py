f7eae62eab7e03888dfa24a0a6e0a10371084f06
hgb926 <hgb926@naver.com>
2024-09-04 22:56:17 +0900

    initial commit

---
+++ python-basic/type.py
@@ -0,0 +1,15 @@
+print("Hello World")
+
+name = "Alice"
+age = 30
+height = 4.1
+is_student = True
+fruits = ["apple", "orange", "pear"]
+print(fruits[0])
+
+point = (10, 20)
+print(point[1])
+
+person = {"name": "Alice", "age": 30, "height": 4.1}
+print(person["name"])
+
