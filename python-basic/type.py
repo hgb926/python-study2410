78abf8a8de03bde3446163a299b565782f193d53
hgb926 <hgb926@naver.com>
2024-09-05 09:32:43 +0900

    type study

--- python-basic/type.py
+++ python-basic/type.py
@@ -1,3 +1,5 @@
+import math
+
 print("Hello World")
 
 name = "Alice"
@@ -12,4 +14,36 @@ print(point[1])
 
 person = {"name": "Alice", "age": 30, "height": 4.1}
 print(person["name"])
+
+if age < 19:
+    print("어린이")
+elif age < 65:
+    print("성인")
+else:
+    print("노인")
+
+for fruit in fruits:
+    print(fruit)
+
+count = 0
+while count < 5:
+    print(count)
+    count += 2
+
+def greet(name):
+    return f"Hello {name}!"
+print(greet("Alice"))
+
+class Dog:
+    def __init__(self, name):
+        self.name = name
+
+    def bark(self):
+        return f"{self.name} say woof!"
+my_dog = Dog("Buddy")
+print(my_dog.bark())
+
+
+print(math.sqrt(16))
+
 
